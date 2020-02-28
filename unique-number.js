634fa96cc4df85ed296950de962aa06b0e8ba743
Harry Chopra <harrychopra@gmail.com>
2020-02-28 00:25:51 +0000

    Done

---
+++ unique-number.js
@@ -0,0 +1,28 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// Using an object
+
+function findUniq(arr) {
+	let numCount = {};
+	for (let i of arr) {
+		if (numCount[i]) {
+			numCount[i]++;
+		} else {
+			numCount[i] = 1;
+		}
+	}
+	for (let j in numCount) {
+		if (numCount[j] === 1) {
+			return j;
+		}
+	}
+}
+
+console.log(findUniq([1, 1, 1, 2, 1, 1]));
+console.log(findUniq([0, 0, 0.55, 0, 0]));
